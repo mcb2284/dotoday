e6cfc0cc47c890fc8a2d9dc8e0e94316865e95e6
Michael Bollinger <mcb2284@columbia.edu>
2023-08-28 23:11:24 -0400

    added nextui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
-      <Component {...pageProps} />
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
     </ClerkProvider>
   );
 };
